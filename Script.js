5152c6e3c3d32eadcd7c1db17602945c09d4dc10
PankovaMS <LmargojayL@gmail.com>
2024-07-17 13:30:12 +0300

    btn, fixes and more

--- Script.js
+++ Script.js
@@ -13,22 +13,50 @@
 //     });
 // });
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// const appendAlert = (message, type) => {
+//   const wrapper = document.createElement('div')
+//   wrapper.innerHTML = [
+//     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+//     `   <div>${message}</div>`,
+//     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+//     '</div>'
+//   ].join('')
 
-  alertPlaceholder.append(wrapper)
-}
+//   alertPlaceholder.append(wrapper)
+// }
 
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Link copied!', 'success')
-  })
-}
+// const alertTrigger = document.getElementById('liveAlertBtn')
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', () => {
+//     appendAlert('Link copied!', 'success')
+//   })
+// }
+// document.getElementById('CopyLink').onclick = CopyFunction;
+// function CopyFunction() {
+//     alert('Link copied');
+// }
+
+// //  
+// ДЛЯ РАБОТЫ КОПИРОВАНИЯ ССЫЛКИ НА СПИСОК В БУФЕР ОБМЕНА 
+// var copyEmailBtn = document.querySelector('.js-emailcopybtn');   
+// copyEmailBtn.addEventListener('click', function(event) {  
+//   // Выборка ссылки с электронной почтой 
+//   var emailLink = document.querySelector('.js-emaillink');  
+//   var range = document.createRange();  
+//   range.selectNode(emailLink);  
+//   window.getSelection().addRange(range);  
+    
+//   try {  
+//     // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+//     var successful = document.execCommand('copy');  
+//     var msg = successful ? 'successful' : 'unsuccessful';  
+//     console.log('Copy email command was ' + msg);  
+//   } catch(err) {  
+//     console.log('Oops, unable to copy');  
+//   }  
+    
+//   // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+//   // removeRange(range) когда это возможно
+//   window.getSelection().removeAllRanges();  
+// }); 
